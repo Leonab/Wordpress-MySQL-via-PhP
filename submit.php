d5a7894c43931c8e13c3e13212498ce94182b083
Nabeel Khawar <Nabeel Khawar>
2016-07-15 14:04:34 +0530

    first

---
+++ submit.php
@@ -0,0 +1,39 @@
+<?php
+error_reporting(-1);
+//  error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
+
+	define( 'ABSPATH', dirname(__FILE__) . '/' );
+
+  require('ABSPATH . wp-load.php' );
+//    require_once('C:\wamp\www\wp-load.php');
+	global $wpdb;
+        if (!empty($_POST)) {
+
+            $table = info;
+            $data = array(
+                'name' => $_POST['name'],         //these completely depend on the structure of MySql table
+                'address' => $_POST['address'],
+				        'place' => $_POST['place']
+            );
+			//print_r($data);
+            $format = array(
+                '%s',
+                '%s'
+            );
+            $success=$wpdb->insert( $table, $data, $format );
+            if($success){
+            echo 'Data has been saved' ;
+}
+else
+	echo 'error occured';
+}
+
+$query = "Select * from `info`";
+
+$return = $wpdb->get_results($query);
+if($return)
+	print_r($return);
+else
+	echo "Some error occured";
+
+?>
